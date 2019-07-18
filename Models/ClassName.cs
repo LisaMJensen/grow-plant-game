a9ec278a0f163ea028121a701fe4d3961d23334d
Lisa Jensen <leese_38@yahoo.com>
2019-07-18 15:30:48 -0700

    Add initial files and code for the plant base stats

--- Models/ClassName.cs
+++ Models/ClassName.cs
@@ -1,1 +1,98 @@
 using System;
+using System.Collections.Generic;
+
+namespace Plantgame.Models
+{
+    class Plant
+    {
+
+        private string _PlantName;
+        private int _WaterLevel;
+        private int _FoodLevel;
+        private int _SunshineLevel;
+        private int _PlantHealth;
+
+        public Plant(string plantName, int waterLevel, int foodLevel, int sunshineLevel, int plantHealth)
+        {
+
+            _PlantName = plantName;
+            _WaterLevel  = waterLevel;
+            _FoodLevel = foodLevel;
+            _SunshineLevel = sunshineLevel;
+            _PlantHealth = plantHealth;
+        }
+
+        public string GetPlantName()
+        {
+            return _PlantName;
+        }
+
+        public int GetWaterLevel()
+        {
+            return _WaterLevel;
+        }
+
+        public int GetFoodLevel()
+        {
+            return _FoodLevel;
+        }
+
+        public int GetSunshineLevel()
+        {
+            return _SunshineLevel;
+        }
+
+        public int GetPlantHealth()
+        {
+            return _PlantHealth;
+        }
+
+        public void SetPlantName(string newPlantName)
+        {
+          _PlantName = newPlantName;
+        }
+
+        public void DetermineNextSteps()
+        {
+            Feed();
+            Water();
+            GiveSun();
+            // UpdatePlantHealth();
+            // IsPlantDead();
+        }
+
+        private void Feed()
+        {
+            _FoodLevel++;
+        }
+
+        private void Water()
+        {
+            _WaterLevel++;
+        }
+
+        private void GiveSun()
+        {
+            _SunshineLevel++;
+        }
+
+        // private void  UpdatePlantHealth()
+        // {
+        //
+        // }
+        //
+        // private string IsPlantDead()
+        // {
+        //
+        // }
+
+
+
+
+
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
+using Plantgame.Models;
 
-class ProjectName
+class Program
 {
-  static void Main()
-  {
-    
-  }
+    public static List<Plant> Plants = new List<Plant>() {};
+
+    public static void ShowPlantStats()
+    {
+        Console.WriteLine("Your Plant's Current Stats:");
+        foreach(Plant newPlant in Plants)
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine(newPlant.GetPlantName());
+
+            Console.WriteLine("Water Level: " + newPlant.GetWaterLevel());
+            Console.WriteLine("Food Level: " + newPlant.GetFoodLevel());
+            Console.WriteLine("Sunshine Level: " + newPlant.GetSunshineLevel());
+            Console.WriteLine("Plant Health: " + newPlant.GetPlantHealth());
+
+        }
+    }
+
+    static void Main()
+    {
+     Console.WriteLine("WELCOME TO PLANT GROW");
+     Console.WriteLine("----------------");
+     Console.WriteLine("Give your plant a name:");
+     string answer = Console.ReadLine();
+     Plant newPlant = new Plant (answer, 20, 20, 20, 50);
+
+     Plants.Add(newPlant);
+     ShowPlantStats();
+    }
+
+
+
+
+
+
+
+
+
 }
